b550cba4c34a4e8f084f7b325711381d13db3b2b
saulfranco14 <saul.franco1420@gmail.com>
2025-03-28 22:49:32 -0600

    Agregamos archivo .eslintignore, incluimos favicon.ico y configuramos el icono en los metadatos del layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Workspace',
   description: 'Workspace in Next.js',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
